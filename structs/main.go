1c0d7170f72ee4e5c8ec9406490e753e83b43ab0
Paraschiv Alexandru Andrei <alexparaschiv1402@gmail.com>
2021-12-21 16:41:29 +0200

    struct add

--- structs/main.go
+++ structs/main.go
@@ -2,20 +2,22 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	/*alex := person{"Alex", "Anderson"}
-	bob := person{firstName: "Bob", lastName: "Bobderson"}
-	fmt.Println(alex)
-	fmt.Println(bob)*/
-
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Bob"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	jim := person{firstName: "Jim",
+		lastName: "Party",
+		contact: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 12345}}
+	fmt.Printf("%+v", jim)
 }

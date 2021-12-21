02b73658d23144fbb71270b57fa7675c52c9b690
Paraschiv Alexandru Andrei <alexparaschiv1402@gmail.com>
2021-12-21 18:22:29 +0200

    new method TO define maps

--- maps/main.go
+++ maps/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-		"blue":  "#0000ff",
-	}
+	//var colors map[string]string
+	colors := make(map[string]string)
+
+	colors["new_color"] = "#23ff21"
+	delete(colors, "new_color")
+
 	fmt.Println(colors)
 }

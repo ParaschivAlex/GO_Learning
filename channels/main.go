f1e5bfd73f8e3a4cee42e108e6fcd8225a516170
Paraschiv Alexandru Andrei <alexparaschiv1402@gmail.com>
2021-12-27 22:17:31 +0200

    Update main.go

--- channels/main.go
+++ channels/main.go
@@ -13,17 +13,24 @@ func main() {
 		"http://golang.org",
 		"http://amazon.com",
 	}
+
+	c := make(chan string) //declarare channel
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c) // primim o valoare de la channel
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + " DOWN!")
+		c <- "Might be down!" //trimit mesaj catre server
 		return
 	}
 	fmt.Println(link + " UP!")
+	c <- "Yea, it's up!"
 	return
 }

67e6963b75cd95b120d3cacca2fa9b4fdbb1fa0d
Paraschiv Alexandru Andrei <alexparaschiv1402@gmail.com>
2021-12-27 14:00:42 +0200

    Update main.go

--- http/main.go
+++ http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
@@ -17,5 +19,15 @@ func main() {
 	/*bs := make([]byte, 99999)
 	resp.Body.Read(bs)
 	fmt.Println(string(bs))*/
-	io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (lw logWriter) Write(bs []byte) (int, error) {
+	//return 1, nil
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
